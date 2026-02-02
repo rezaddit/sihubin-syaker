9fa5dd44331dc9a35c89d2a190a533adaa891488
rezae <mrecraxk@gmail.com>
2026-02-02 10:56:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/landing/Home';
 import ServiceDetail from './pages/service/ServiceDetail'; // Import ini
 import Login from './pages/auth/Login'; // Import page baru
 import Register from './pages/auth/Register';
-import SelectRole from './pages/auth/SelectRole';
+//import SelectRole from './pages/auth/SelectRole';
 import UserDashboard from './pages/dashboard/UserDashboard';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Route path="/layanan/:id" element={<ServiceDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/select-role" element={<SelectRole />} />
+        {/* <Route path="/select-role" element={<SelectRole />} /> */}
         <Route path="/dashboard" element={<UserDashboard />} />
       </Routes>
     </Router>
